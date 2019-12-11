4c5a13026520d50c0413c46c1aa1536828e9f145
Lucas Cardoso <englucas@outlook.com>
2019-12-11 14:56:50 -0300

    updated shop data with new data, updated cart utils to group items in cart

--- src/store/actions/cart.js
+++ src/store/actions/cart.js
@@ -3,3 +3,24 @@ import { CartActionTypes } from ".";
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantify: cartItem.quantify + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantify: 1 }];
+};
